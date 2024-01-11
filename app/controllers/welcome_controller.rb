095d2352e90a434a97032c67cd14fca12d95630a
Almad <lukas.linhart@apollo.io>
2024-01-11 19:03:48 +0100

    More untested code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
     if false
       puts "Oh no, an untested and unreachable code!"
+      puts "it keeps decreasing!"
+      puts "oh no somebody call TDD SWAT"
     end
   end
 end
