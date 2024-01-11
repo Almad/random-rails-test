e109821e4641db6a9a00a7add4c9761cf9a3b506
Almad <lukas.linhart@apollo.io>
2024-01-11 19:42:57 +0100

    Add moar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
       puts "Oh no, an untested and unreachable code!"
       puts "it keeps decreasing!"
       puts "oh no somebody call TDD SWAT"
+      puts "EXTERMINATE BEFORE IT SPAWN()s"
     end
     if true
       puts "covered"
@@ -12,5 +13,6 @@ class WelcomeController < ApplicationController
   
   def random
     puts "this is untested totally, right?"
+    puts "more untested boilerplate"
   end
 end
